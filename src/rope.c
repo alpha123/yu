dc6c718bd9edf2f89d836ba550418a1a96fe1bd5
Peter Cannici <turkess123@gmail.com>
2016-03-24 22:54:28 -0600

    Flesh out the API a bit more

--- src/rope.c
+++ src/rope.c
@@ -5,4 +5,14 @@
 
 #include "rope.h"
 
+YU_MALLOC_LIKE
+yu_rope *rope_new(yu_allocator *mctx, yu_rand *rng) {
+  yu_rope *r = yu_xalloc(mctx, 1, sizeof(yu_rope));
+  r->mem_ctx = mctx;
+  r->rng = rng;
+  return r;
+}
 
+void rope_free(yu_rope *r) {
+  yu_free(r->mem_ctx, r);
+}
--- src/rope.h
+++ src/rope.h
@@ -19,40 +19,62 @@ typedef enum {
 
 typedef size_t (* yu_rope_fn)(u8 *out_bytes, size_t start, size_t end, void *data);
 
+struct yu_rope;
+struct yu_rnode;
+
+union yu_rnode_val {
+  struct {
+    struct yu_rnode *left, *right;
+    u8 priority;
+  } join;
+
+  struct {
+    yu_rope_fn func;
+    void *udata;
+  } fn;
+
+  FILE *stream;
+
+  struct {
+    struct yu_rnode *start_node, *end_node;
+    size_t start, end;
+  } slice;
+
+  struct {
+    size_t len;
+    u8 *bytes;
+  } buf;
+};
+
 struct yu_rnode {
-  union {
-    struct {
-      struct yu_rnode *left, *right;
-      u8 priority;
-    } join;
-
-    struct {
-      yu_rope_fn func;
-      void *udata;
-    } fn;
-
-    FILE *stream;
-
-    u8 *bytes;  // length is stored as a size_t allocated immediately in front
-                // of `bytes`.
-  } val;
+  union yu_rnode_val val;
   yu_rope_node_type what;
 };
 
 typedef struct {
-  sfmt_t rng;  // random numbers for treap priorities
+  yu_rand *rng;  // random numbers for treap priorities
   yu_allocator *mem_ctx;
   struct yu_rnode *root;
 } yu_rope;
 
+YU_MALLOC_LIKE
 yu_rope *rope_new(yu_allocator *mctx);
+void rope_free(yu_rope *r);
+
+yu_rope *rope_copy(yu_rope *r);
+
 void rope_add_node(yu_rope *r, struct yu_rnode *n);
-// In general u8* parameters are restrict; since u8 is most likely a char type
-// it can alias anything unless we explicitly say not to.
-void rope_cat_bytes(yu_rope *r, const u8 * restrict bytes, size_t sz, bool take_ownership);
+
+// In general u8 * parameters should be `restrict`; since u8 is most likely a
+// char type it can alias anything (including the rope object) unless we
+// explicitly say not to.
+void rope_cat_bytes(yu_rope *r, const u8 * restrict bytes, size_t sz, bool copy);
 void rope_cat_func(yu_rope *r, yu_rope_fn func, void *data);
 void rope_cat_stream(yu_rope *r, FILE *stream);
 
+// The rope returned from this function is tied to the lifetime of the rope the
+// slice was taken from. Use rope_copy() if you want to make an independent
+// copy.
 yu_rope *rope_slice(yu_rope *r, size_t start, size_t end);
 
 size_t rope_write(yu_rope *r, FILE *out);
