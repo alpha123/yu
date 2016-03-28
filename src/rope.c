a838f47cc532e737298e2a41b89c6f05dd2e2afa
alpha123 <turkchess123@gmail.com>
2016-03-28 14:13:37 -0600

    Minor rope changes

--- src/rope.c
+++ src/rope.c
@@ -5,4 +5,13 @@
 
 #include "rope.h"
 
+rope *rope_new(yu_allocator *mctx) {
+  rope *r = yu_xalloc(1, sizeof(rope));
+  r->mem_ctx = mctx;
+  sfmt_init_genrand(&r->rng, yu_sys_random());
+  return r;
+}
 
+void rope_free(rope *r) {
+  yu_free(r->mem_ctx, r);
+}
