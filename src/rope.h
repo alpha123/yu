1d5dd22cac7b864d43294bad529d834460db3ef8
alpha123 <turkchess123@gmail.com>
2016-03-28 18:25:56 -0600

    Replace WIP rope with modified librope https://github.com/josephg/librope

--- src/rope.h
+++ src/rope.h
@@ -1,59 +1,149 @@
 /**
+ * Copyright (c) 2015 Joseph Gentle
  * Copyright (c) 2016 Peter Cannici
- * Licensed under the MIT (X11) license. See LICENSE.
+ *
+ * This library implements a heavyweight utf8 string type with fast
+ * insert-at-position and delete-at-position operations.
+ *
+ * It uses skip lists instead of trees. Trees might be faster - who knows?
+ *
+ * Ropes are not syncronized. Do not access the same rope from multiple threads
+ * simultaneously.
  */
 
 #pragma once
 
 #include "yu_common.h"
 
-typedef enum { ROPE_NODE_BYTES, ROPE_NODE_SLICE, ROPE_NODE_STREAM, ROPE_NODE_FUNC } rope_node_type;
+// These two magic values seem to be approximately optimal given the benchmark
+// in tests.c which does lots of small inserts.
 
-typedef size_t (* rope_fn)(u8 *out_bytes, size_t start, size_t end, void *data);
+// Must be <= UINT16_MAX. Benchmarking says this is pretty close to optimal
+// (tested on a mac using clang 4.0 and x86_64).
+#ifndef ROPE_NODE_STR_SIZE
+#define ROPE_NODE_STR_SIZE 136
+#endif
 
-struct rope_node {
-  union {
-    struct {
-      struct rope_node *ref;
-      size_t start, end;
-    } slice;
+// The likelyhood (%) a node will have height (n+1) instead of n
+#ifndef ROPE_BIAS
+#define ROPE_BIAS 25
+#endif
 
-    struct {
-      struct rope_node *left, *right, *parent;
-      u8 priority;
-    } join;
+// The rope will stop being efficient after the string is 2 ^ ROPE_MAX_HEIGHT
+// nodes.
+#ifndef ROPE_MAX_HEIGHT
+#define ROPE_MAX_HEIGHT 60
+#endif
 
-    struct {
-      rope_fn func;
-      void *udata;
-    } fn;
+struct rope_node_t;
 
-    FILE *stream;
+// The number of characters in str can be read out of nexts[0].skip_size.
+typedef struct {
+  // The number of _characters_ between the start of the current node
+  // and the start of next.
+  size_t skip_size;
 
-    struct {
-      size_t len;
-      u8 *bytes;
-    } buf;
-  } val;
-  rope_node_type what;
-  u8 depth;
-};
+  // For some reason, librope runs about 1% faster when this next pointer is
+  // exactly _here_ in the struct.
+  struct rope_node_t *node;
+} rope_skip_node;
+
+typedef struct rope_node_t {
+  uint8_t str[ROPE_NODE_STR_SIZE];
+
+  // The number of bytes in str in use
+  uint16_t num_bytes;
+
+  // This is the number of elements allocated in nexts.
+  // Each height is 1/2 as likely as the height before. The minimum height is 1.
+  uint8_t height;
+
+  rope_skip_node nexts[];
+} rope_node;
 
 typedef struct {
-  sfmt_t rng;  // random numbers for treap priorities
+  // The total number of characters in the rope.
+  size_t num_chars;
+
+  // The total number of bytes which the characters in the rope take up.
+  size_t num_bytes;
+
   yu_allocator *mem_ctx;
-  struct rope_node *root;
+  sfmt_t *rng;
+
+  // The first node exists inline in the rope structure itself.
+  rope_node head;
 } rope;
 
-rope *rope_new(yu_allocator *mctx);
-void rope_add_node(rope *r, struct rope_node *n);
-// In general u8* parameters are `restrict`; since u8 is most likely a char type
-// it can alias anything unless we explicitly say not to.
-void rope_cat_bytes(rope *r, const u8 * restrict bytes, size_t sz, bool take_ownership);
-void rope_cat_func(rope *r, rope_fn func, void *data);
-void rope_cat_stream(rope *r, FILE *stream);
+// Create a new rope with no contents
+rope *rope_new(yu_allocator *mctx, sfmt_t *rng);
 
-rope *rope_slice(rope *r, size_t start, size_t end);
+// Create a new rope containing a copy of the given string. Shorthand for
+// r = rope_new(); rope_insert(r, 0, str);
+rope *rope_new_with_utf8(yu_allocator *mctx, sfmt_t *rng, const uint8_t *str);
 
-size_t rope_write(rope *r, FILE *out);
-size_t rope_to_bytes(rope *r, u8 **out);
+// Make a copy of an existing rope
+rope *rope_copy(const rope *r);
+
+// Free the specified rope
+void rope_free(rope *r);
+
+// Get the number of characters in a rope
+size_t rope_char_count(const rope *r);
+
+// Get the number of bytes which the rope would take up if stored as a utf8
+// string
+size_t rope_byte_count(const rope *r);
+
+// Copies the rope's contents into a utf8 encoded C string. Also copies a
+// trailing '\0' character.
+// Returns the number of bytes written, which is rope_byte_count(r) + 1.
+size_t rope_write_cstr(rope *r, uint8_t *dest);
+
+// Create a new C string which contains the rope. The string will contain
+// the rope encoded as utf8, followed by a trailing '\0'.
+// Use rope_byte_count(r) to get the length of the returned string.
+uint8_t *rope_create_cstr(rope *r);
+
+// If you try to insert data into the rope with an invalid UTF8 encoding,
+// nothing will happen and we'll return ROPE_INVALID_UTF8.
+typedef enum { ROPE_OK, ROPE_INVALID_UTF8 } ROPE_RESULT;
+
+// Insert the given utf8 string into the rope at the specified position.
+ROPE_RESULT rope_insert(rope *r, size_t pos, const uint8_t *str);
+
+// Delete num characters at position pos. Deleting past the end of the string
+// has no effect.
+void rope_del(rope *r, size_t pos, size_t num);
+
+// This macro expands to a for() loop header which loops over the segments in a
+// rope.
+//
+// Eg:
+//  rope *r = rope_new_with_utf8(str);
+//  ROPE_FOREACH(r, iter) {
+//    printf("%s", rope_node_data(iter));
+//  }
+#define ROPE_FOREACH(rope, iter) \
+  for (rope_node *iter = &(rope)->head; iter != NULL; iter = iter->nexts[0].node)
+
+// Get the actual data inside a rope node.
+static inline uint8_t *rope_node_data(rope_node *n) {
+  return n->str;
+}
+
+// Get the number of bytes inside a rope node. This is useful when you're
+// looping through a rope.
+static inline size_t rope_node_num_bytes(rope_node *n) {
+  return n->num_bytes;
+}
+
+// Get the number of characters inside a rope node.
+static inline size_t rope_node_chars(rope_node *n) {
+  return n->nexts[0].skip_size;
+}
+
+
+// For debugging.
+void _rope_check(rope *r);
+void _rope_print(rope *r);
--- test/test_rope.c
+++ test/test_rope.c
@@ -5,14 +5,40 @@
 
 #include "test.h"
 
+#include "rope.h"
+
 #define SETUP \
-    ...
+  TEST_GET_ALLOCATOR(mctx); \
+  sfmt_t rng; \
+  sfmt_init_gen_rand(&rng, 126155); \
+  rope *r = rope_new((yu_allocator *)&mctx, &rng);
 
 #define TEARDOWN \
-    ...
+  rope_free(r); \
+  yu_alloc_ctx_free(&mctx);
 
 #define LIST_ROPE_TESTS(X) \
-    X(...)
+  X(create_str, "Initializing a rope with a string should create a rope with the same contents") \
+  X(unicode_str, "Code point counts should accurately be reported for UTF-8 strings")
 
+
+TEST(create_str)
+  const char s[] = "If you have time to plan a beautiful death, you have time to live beautifully to the end.";
+  rope *r2 = rope_new_with_utf8((yu_allocator *)&mctx, &rng, (const u8 *)s);
+  PT_ASSERT_EQ(rope_byte_count(r2), strlen(s));
+  PT_ASSERT_EQ(rope_char_count(r2), strlen(s));
+  char out_s[sizeof s];
+  PT_ASSERT_EQ(rope_write_cstr(r2, (u8 *)out_s), strlen(s) + 1);
+  PT_ASSERT_STR_EQ(out_s, s);
+  rope_free(r2);
+END(create_str)
+
+TEST(unicode_str)
+  // File must be encoded as UTF-8 for this test to work.
+  const char s[] = "年年有余";
+  rope *r2 = rope_new_with_utf8((yu_allocator *)&mctx, &rng, (const u8 *)s);
+  PT_ASSERT_EQ(rope_byte_count(r2), strlen(s));
+  PT_ASSERT_EQ(rope_char_count(r2), 4u);
+END(unicode_str)
 
 SUITE(rope, LIST_ROPE_TESTS)
